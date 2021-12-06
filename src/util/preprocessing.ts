06513d93ac9bf47e11662d766208245349e27b90
aprifield <aprifield@gmail.com>
2021-12-06 18:59:22 +0900

    modify text processing script

--- src/util/preprocessing.ts
+++ src/util/preprocessing.ts
@@ -2,17 +2,26 @@ import { PreprocessingHistoryEvent } from '@/types/history-event';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const dummy = (text: string, event: PreprocessingHistoryEvent) => {
+  const addPrefix = (prefix: string | number) => {
+    const map = (str: string, index: number, lines: string[]) =>
+      index + 1 === lines.length && !str
+        ? ''
+        : +prefix
+        ? (index + 1 + '').padStart(+prefix, '0') + ': ' + str
+        : prefix + str;
+    const eol = text.match(/\r\n|\n|\r/);
+    return eol
+      ? text
+          .split(eol[0])
+          .map(map)
+          .join(eol[0])
+      : map(text, 0, [text]);
+  };
+
+  text = text.replaceAll('Javascript', 'JavaScript');
+
   if (event.ctrlKey) {
     event.preventPaste(); // Call this if you don't want to paste.
-  }
-  if (event.shiftKey) {
-    const m = text.match(/\r\n|\n|\r/);
-    return m
-      ? text
-          .split(m[0])
-          .map(str => '> ' + str)
-          .join(m[0])
-      : '> ' + text;
   }
   if (event.key === 'u') {
     return text.toUpperCase();
@@ -20,24 +29,46 @@ const dummy = (text: string, event: PreprocessingHistoryEvent) => {
   if (event.key === 'l') {
     return text.toLowerCase();
   }
+  if (event.key === 'c') {
+    return text.replace(/_(.)/g, (match, p1) => p1.toUpperCase()); // To camelCase
+  }
+  if (event.key === 's' || event.key === 'S') {
+    text = text.replace(/([A-Z])/g, match => '_' + match.toLowerCase()); // To snake_case
+    return event.key === 'S' ? text.toUpperCase() : text;
+  }
+  if (event.key === 'q' || event.shiftKey) {
+    return addPrefix('> '); // Add quote
+  }
+  if (event.key && 1 <= +event.key && +event.key <= 9) {
+    return addPrefix(+event.key); // Add line number
+  }
   if (event.key === 'd') {
-    return JSON.stringify({ event, text });
+    return JSON.stringify({ event, text }); // For debug
   }
   return text;
 };
 
 export default String.raw`(text: string, event: PreprocessingHistoryEvent) => {
+  const addPrefix = (prefix: string | number) => {
+    const map = (str: string, index: number, lines: string[]) =>
+      index + 1 === lines.length && !str
+        ? ''
+        : +prefix
+        ? (index + 1 + '').padStart(+prefix, '0') + ': ' + str
+        : prefix + str;
+    const eol = text.match(/\r\n|\n|\r/);
+    return eol
+      ? text
+          .split(eol[0])
+          .map(map)
+          .join(eol[0])
+      : map(text, 0, [text]);
+  };
+
+  text = text.replaceAll('Javascript', 'JavaScript');
+
   if (event.ctrlKey) {
     event.preventPaste(); // Call this if you don't want to paste.
-  }
-  if (event.shiftKey) {
-    const m = text.match(/\r\n|\n|\r/);
-    return m
-      ? text
-          .split(m[0])
-          .map(str => '> ' + str)
-          .join(m[0])
-      : '> ' + text;
   }
   if (event.key === 'u') {
     return text.toUpperCase();
@@ -45,10 +76,26 @@ export default String.raw`(text: string, event: PreprocessingHistoryEvent) => {
   if (event.key === 'l') {
     return text.toLowerCase();
   }
+  if (event.key === 'c') {
+    return text.replace(/_(.)/g, (match, p1) => p1.toUpperCase()); // To camelCase
+  }
+  if (event.key === 's' || event.key === 'S') {
+    text = text.replace(/([A-Z])/g, match => '_' + match.toLowerCase()); // To snake_case
+    return event.key === 'S' ? text.toUpperCase() : text;
+  }
+  if (event.key === 'q' || event.shiftKey) {
+    return addPrefix('> '); // Add quote
+  }
+  if (event.key && 1 <= +event.key && +event.key <= 9) {
+    return addPrefix(+event.key); // Add line number
+  }
   if (event.key === 'd') {
-    return JSON.stringify({ event, text });
+    return JSON.stringify({ event, text }); // For debug
   }
   return text;
 }`
+  .replaceAll(': string | number', '')
+  .replaceAll(': string[]', '')
   .replaceAll(': string', '')
+  .replaceAll(': number', '')
   .replaceAll(': PreprocessingHistoryEvent', '');
